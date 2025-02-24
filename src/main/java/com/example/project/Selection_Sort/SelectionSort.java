3ef2a83bfc319dd2ddaa807443f5870a9f9cc022
Cormac-OK <cormaco@nycstudents.net>
2025-02-24 22:49:24 +0000

    All tests passing

--- src/main/java/com/example/project/Selection_Sort/SelectionSort.java
+++ src/main/java/com/example/project/Selection_Sort/SelectionSort.java
@@ -8,14 +8,40 @@ public class SelectionSort {
 
     // PART A. implementing selection sort
     public static int[] selectionSort(int[] elements) {
-       
+        for (int i = 0; i < elements.length-1; i ++){
+            int min = elements[i + 1];
+            int minIndex = i + 1;
+            for (int j = i+1; j < elements.length; j ++){
+                if (elements[j] < min){
+                    min = elements[j];
+                    minIndex = j;
+                }
+            }
+            if(min < elements[i]){
+                elements[minIndex] = elements[i];
+                elements[i] = min;
+            }
+        }
         return elements;
     }
 
 
     // PART B. sorting a 1000-word list
     public static ArrayList<String> selectionSortWordList(ArrayList<String> words) {
-      
+        for (int i = 0; i < words.size()-1; i ++){
+            String min = words.get(i + 1);
+            int minIndex = i + 1;
+            for (int j = i+1; j < words.size(); j ++){
+                if (words.get(j).compareTo(min) < 0){
+                    min = words.get(j);
+                    minIndex = j;
+                }
+            }
+            if(min.compareTo(words.get(i)) < 0){
+                words.set(minIndex, words.get(i));
+                words.set(i, min);
+            }
+        }
         return words;
     }
 
